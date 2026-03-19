794d793ebb57d0576c23336f40547d89b949d349
petryashovis <toolzz@yandex.ru>
2026-03-19 19:12:25 +0300

    cli.dart - version

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 const version = '0.0.1';
 
 void main(List<String> arguments) {
@@ -5,9 +7,25 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     printUsage();
   }
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Current article title: $articleTitle');
 }
 
 void printUsage() {
