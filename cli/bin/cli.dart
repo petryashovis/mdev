e6374b35287fd387d308a3c011e8fc9bbb600b6e
petryashovis <toolzz@yandex.ru>
2026-03-19 19:02:01 +0300

    implemented help arg

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,9 +1,17 @@
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Hello Dart!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else {
+    printUsage();
   }
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
 }
