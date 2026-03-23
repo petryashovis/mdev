b4cc357ea6d821693dcaf4afb48c396fe1ed33ad
petryashovis <toolzz@yandex.ru>
2026-03-23 14:17:17 +0300

    add arguments.dart with new classes and methods

---
+++ command_runner/lib/src/arguments.dart
@@ -0,0 +1,75 @@
+import 'dart:collection';
+import '../command_runner.dart';
+
+enum OptionType { flag, option }
+
+abstract class Argument {
+  String get name;
+  String? get help;
+
+  Object? get defaultValue;
+  String? get valueHelp;
+
+  String get usage;
+}
+
+class Option extends Argument {
+  Option(
+    this.name, {
+    required this.type,
+    this.help,
+    this.abbr,
+    this.defaultValue,
+    this.valueHelp,
+  });
+
+  @override
+  final String name;
+
+  final OptionType type;
+
+  @override
+  final String? help;
+
+  final String? abbr;
+
+  @override
+  final Object? defaultValue;
+
+  @override
+  final String? valueHelp;
+
+  @override
+  String get usage {
+    if (abbr != null) {
+      return '-$abbr,--$name: $help';
+    }
+
+    return '--$name: $help';
+  }
+}
+
+abstract class Command extends Argument {
+  @override
+  String get name;
+
+  String get description;
+
+  bool get requiresArgument => false;
+
+  late CommandRunner runner;
+
+  @override
+  String? help;
+
+  @override
+  String? defaultValue;
+
+  @override
+  String? valueHelp;
+
+  final List<Option> _options = [];
+
+  UnmodifiableSetView<Option> get options =>
+      UnmodifiableSetView(_options.toSet());
+}
