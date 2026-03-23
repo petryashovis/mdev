ba239981e687fdd6c3f093f5791909f5b282bf46
petryashovis <toolzz@yandex.ru>
2026-03-23 14:37:09 +0300

    Update command_runner library exports

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -1,10 +1,6 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
 export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
 export 'src/help_command.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/exceptions.dart';
